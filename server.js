c47fa49a653f0786dc3c1cbcb3776ddec9bb73cb
zichenma <zma@liaisionedu.com>
2021-05-19 12:41:28 -0400

    6 initialize express server

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 4444;
+
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+});
+
+
