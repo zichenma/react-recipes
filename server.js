b1dcab6c6d198e002156fed3ad000ba10d1b99f9
zichenma <zma@liaisionedu.com>
2021-05-19 13:37:28 -0400

    7 create mongoDB connection

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config({path: 'variables.env'});
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log('DB connected'))
+.catch(err => console.error(err));
+
 const app = express();
 
 const PORT = process.env.PORT || 4444;
