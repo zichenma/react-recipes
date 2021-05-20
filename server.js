d4e3f45176aca274e1d9f47f6194ecf2fb669835
zichenma <zma@liaisionedu.com>
2021-05-20 01:16:39 -0400

    9-Add-apollo-express-middleware

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 require('dotenv').config({path: 'variables.env'});
 
 const Recipe = require('./models/Recipe');
 const User = require('./models/User');
+
+// Bring in GraphQL Express middleware
+
+const { graphiqlExpress, graphqlExpress } = require('apollo-server-express');
+const { makeExecutableSchema } = require('graphql-tools');
+
+const { typeDefs } = require('./schema');
+const { resolvers } = require('./resolvers');
+
+// Create graphQL schema
+const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers,
+})
 
 // connect to database
 
@@ -15,6 +30,20 @@ mongoose.connect(process.env.MONGO_URI, {autoIndex: false})
 // Initialize application
 
 const app = express();
+
+// Greate GraphiQL application
+
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql'}));
+
+// Connect schemas with GraphQL
+
+app.use('/graphql', graphqlExpress({
+    schema,
+    context: {
+        Recipe,
+        User
+    }
+}))
 
 const PORT = process.env.PORT || 4444;
 
