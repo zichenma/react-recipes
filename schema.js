08a6937c2e281ddd562691fbd3959f48bf39378c
zichenma <zma@liaisionedu.com>
2021-05-20 01:27:17 -0400

    10-create-graphQL-schema

--- schema.js
+++ schema.js
@@ -1,1 +1,30 @@
-exports.typeDefs = ``;
+// ! means required
+exports.typeDefs = `
+    type Recipe {
+        name: String!
+
+        category: String!
+
+        description: String!
+
+        instructions: String!
+
+        createdDate: String
+
+        likes: Int
+
+        username: String
+    }
+
+    type User {
+        username: String! @unique
+
+        password: String!
+
+        email: String!
+
+        joinDate: String
+
+        favorites: []
+    }
+`;
