7c0e9e4e98cafd31dc365cc8713196e2258da67f
zichenma <zma@liaisionedu.com>
2021-05-21 01:50:47 -0400

    16 Add client dependencies and apolloClient

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/client';
 
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4444/graphql'
+})
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
