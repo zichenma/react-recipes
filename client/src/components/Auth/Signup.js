6dc8214edf50b9928291f1f12827ffc5bd151f3c
zichenma <zma@liaisionedu.com>
2021-05-22 13:13:05 -0400

    22 add react router dom

--- client/src/components/Auth/Signup.js
+++ client/src/components/Auth/Signup.js
@@ -3,7 +3,16 @@ import React from 'react';
 class Signup extends React.Component {
     render() {
         return (
-            <div>Signup</div>
+            <div className="App">
+                <h2 className="App">Signup</h2>
+                <form className="form">
+                    <input type="text" name="username" placeholder="username" />
+                    <input type="email" name="email" placeholder="email address" />
+                    <input type="password" name="password" placeholder="password" />
+                    <input type="password" name="passwordConfirmation" placeholder="Confirm Password" />
+                    <button type="submit" className="button-primary">Submit</button>
+                </form>
+            </div>
         )
     }
 }
