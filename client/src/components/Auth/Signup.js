08b68827e2dccfb82c5137d8ccc31728fad6e6ac
zichenma <zma@liaisionedu.com>
2021-05-23 12:11:14 -0400

    25 add mutation component to signup

--- client/src/components/Auth/Signup.js
+++ client/src/components/Auth/Signup.js
@@ -1,34 +1,49 @@
-import React from 'react';
+import { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { SIGNUP_USER } from '../../queries';
 
-class Signup extends React.Component {
-    
-    state = {
+
+
+function Signup() {
+    const [formData, setFormData] = useState({
         username: '',
         email: '',
         password: '',
         passwordConfirmation: ''
-    };
+    });
+    
+    const [signupUser, { data }] = useMutation(SIGNUP_USER);
 
-    handleChange = event => {
+    const handleChange = event => {
         const { name, value } = event.target;
-        this.setState({ [name] : value} );
+        setFormData({...formData, [name] : value });
     }
 
-    render() {
-        const { username, email, password, passwordConfirmation } = this.state;
-        return (
-            <div className="App">
-                <h2 className="App">Signup</h2>
-                <form className="form">
-                    <input type="text" name="username" placeholder="username" value={username} onChange={this.handleChange}/>
-                    <input type="email" name="email" placeholder="email address" value={email} onChange={this.handleChange}/>
-                    <input type="password" name="password" placeholder="password" value={password} onChange={this.handleChange}/>
-                    <input type="password" name="passwordConfirmation" placeholder="Confirm Password" value={passwordConfirmation} onChange={this.handleChange}/>
+    const handleSubmit = event => {
+        event.preventDefault();
+        const { username, email, password } = formData;
+        signupUser({variables : { username, email, password } });
+        setFormData({
+            username: '',
+            email: '',
+            password: '',
+            passwordConfirmation: ''
+        });
+    }
+
+    return (
+        <div className="App">
+            <h2 className="App">Signup</h2>
+                <form className="form" onSubmit={handleSubmit}>
+                    <input type="text" name="username" placeholder="username" value={formData.username} onChange={handleChange}/>
+                    <input type="email" name="email" placeholder="email address" value={formData.email} onChange={handleChange}/>
+                    <input type="password" name="password" placeholder="password" value={formData.password} onChange={handleChange}/>
+                    <input type="password" name="passwordConfirmation" placeholder="Confirm Password" value={formData.passwordConfirmation} onChange={handleChange}/>
                     <button type="submit" className="button-primary">Submit</button>
                 </form>
-            </div>
-        )
-    }
+        </div>
+    )
 }
+
 
 export default Signup;
--- client/src/queries/index.js
+++ client/src/queries/index.js
@@ -1,5 +1,8 @@
 import { gql } from '@apollo/client';
 
+
+/* Recipes Queries */
+
 export const GET_ALL_RECIPES = gql`
 query {
     getAllRecipes {
@@ -11,4 +14,18 @@ query {
         createdDate
     }
 }
+`;
+
+/* Recipes Mutations */
+
+/* User Queries */
+
+/* User Mutations */
+
+export const SIGNUP_USER = gql`
+mutation ($username: String!, $email: String!, $password: String!) {
+    signupUser(username: $username, email: $email, password: $password) {
+      token
+    }
+  }
 `;
