8b533bdf28e731dad29dcbcc81d3c8dde8adb9cd
zichenma <zma@liaisionedu.com>
2021-06-06 10:49:28 -0400

    39 fixed id issue and remove server.copy

--- client/src/components/Recipe/RecipeItem.js
+++ client/src/components/Recipe/RecipeItem.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const RecipeItem = recipe => {
-    const { id, name, category } = recipe.recipe;
+    const { _id, name, category } = recipe.recipe;
     return (
       <ul>
         <li> 
-            <Link to={`/recipes/${id}`}>
+            <Link to={`/recipes/${_id}`}>
               <h4>{ name }</h4>
             </Link>
             <p><strong>{ category }</strong></p>
--- client/src/components/Recipe/RecipePage.js
+++ client/src/components/Recipe/RecipePage.js
@@ -4,6 +4,7 @@ import { useRouteMatch } from 'react-router-dom';
 const RecipePage = () => {
     const match = useRouteMatch();
     const { _id } = match.params;
+    console.log(_id);
     return (
         <div>Recipe Page</div>
     )
