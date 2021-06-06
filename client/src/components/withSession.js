b478713fd1a2813ac90185ff934eadf21ecb52c0
zichenma <zma@liaisionedu.com>
2021-06-06 00:03:36 -0400

    37 implement signout button

--- client/src/components/withSession.js
+++ client/src/components/withSession.js
@@ -8,7 +8,6 @@ import { GET_CURRENT_USER } from '../queries';
 const withSession = Component => props => {
     const { data, loading, refetch } = useQuery(GET_CURRENT_USER);
     if (loading) return null;
-    console.log('front end', data);
     return <Component {...props} refetch={refetch} session={data}/>;
 };
 
