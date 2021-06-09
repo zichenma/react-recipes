522bc98ece91ac395639eb4b5347931120e6783e
zichenma <zma@liaisionedu.com>
2021-06-09 10:58:50 -0400

    43 implement addRecipe mutation on client

--- client/src/components/Recipe/AddRecipe.js
+++ client/src/components/Recipe/AddRecipe.js
@@ -1,21 +1,53 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useMutation } from '@apollo/client';
+import { ADD_RECIPE } from '../../queries';
+import Error from '../../components/Error';
 
-const AddRecipe = () => {
+const AddRecipe = ({ session }) => {
     const [recipeInfo, setRecipeInfo] = useState({
         name : '',
         instructions : '',
         category : 'Breakfast',
         description: '',
-        username: ''
     });
+
+    const [username, setUsername] = useState('');
+    const [gqlError, setError] = useState('');
+    const [gqlLoading, setGqlLoading] = useState(false);
+
+    const [ mutate ] = useMutation(ADD_RECIPE);
+    const { name, category, description, instructions } = recipeInfo;
+    
+
+    useEffect(() => {
+        console.log(session.getCurrentUser)
+        session.getCurrentUser && setUsername(session.getCurrentUser.username)
+    },[username])
+
     const handleChange = event => {
        const { name, value } = event.target;
        setRecipeInfo({...recipeInfo, [name] : value});
     }
-    const { name, category, description, instructions } = recipeInfo;
+
+    const handleSubmit = async event => {
+        event.preventDefault();
+        try {
+          const { data, loading } = await mutate({variables : {  name, category, description, instructions, username } });
+          console.log(data);
+          setGqlLoading(loading);
+        } catch (e) {
+            setError(e);
+        }
+    }
+    
+    const validateForm = () => {
+        const isInvalid = !name || !category || !description || !instructions;
+        return isInvalid;
+    }
+
     return (<div className="App">
         <h2 className="App">Add Recipe</h2>
-        <form className="form">
+        <form className="form" onSubmit={(event) => handleSubmit(event)}>
             <input type="text" name="name" placeholder="Recipe Name" value={name} onChange={handleChange}/>
             <select name="category" value={category} onChange={handleChange}>
                 <option value="Breakfast">Breakfast</option>
@@ -25,7 +57,8 @@ const AddRecipe = () => {
             </select>
             <input type="text" name="description" placeholder="Add description" value={description} onChange={handleChange}/>
             <textarea type="text" name="instructions" placeholder="Add instructions" value={instructions} onChange={handleChange}></textarea>
-            <button type="submit" className="button-primary">Submit</button>
+            <button type="submit" className="button-primary" disabled={ gqlLoading || validateForm() }>Submit</button>
+            { gqlError && <Error error={ gqlError } />}
         </form>
     </div>)
 }
--- client/src/queries/index.js
+++ client/src/queries/index.js
@@ -28,6 +28,30 @@ export const GET_RECIPE = gql`
   }
 `;
 
+export const ADD_RECIPE = gql`
+  mutation(
+    $name: String!, 
+    $description: String!, 
+    $category: String!, 
+    $instructions: String!, 
+    $username: String) {
+    addRecipe(
+      name: $name, 
+      description: $description, 
+      category: $category, 
+      instructions: $instructions, 
+      username: $username) {
+        _id
+        name
+        category
+        description
+        instructions
+        createdDate
+        likes
+    }
+  }
+`;
+
 /* User Queries */
 
 export const GET_CURRENT_USER = gql`
