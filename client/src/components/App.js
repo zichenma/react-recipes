695ebb2d13e6bfbbcc295c207b8e5f4be653d003
zichenma <zma@liaisionedu.com>
2021-06-14 00:07:17 -0400

    65 Add imageUrl

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -8,11 +8,12 @@ function App() {
   const { loading, error, data } = useQuery(GET_ALL_RECIPES);
   if (loading) return <p>Loading ...</p>;
   if (error) return <p>Error</p>;
-
   return (
       <div className="App">
-        <h1>Home</h1>
-        <ul>
+        <h1 className="main-title">
+          Find Recipes You <strong>Love</strong>
+        </h1>
+        <ul className="cards">
           {
             data.getAllRecipes.map(recipe => (
               <RecipeItem key={ recipe._id } recipe={ recipe } />
--- client/src/components/Recipe/RecipeItem.js
+++ client/src/components/Recipe/RecipeItem.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+
+
+const imageStyle = imageUrl => ({
+  backgroundImage: `${imageUrl}`,
+  backgroundPosition: 'center center',
+  backgroundSize: 'cover',
+  backgroundRepeat: 'no-repeat'
+})
+
 const RecipeItem = recipe => {
-    const { _id, name, category } = recipe.recipe;
+    const { _id, imageUrl, name, category } = recipe.recipe;
     return (
-      <ul>
-        <li> 
+        <li
+          style={imageStyle(imageUrl)} 
+          className="card"
+        >
+        <span className={ category }> 
+            { category } 
+        </span>
+        <div className="card-text">
             <Link to={`/recipes/${_id}`}>
               <h4>{ name }</h4>
             </Link>
-            <p><strong>{ category }</strong></p>
+        </div>
         </li>
-      </ul>
     )
 }
 export default RecipeItem;
